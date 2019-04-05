0ad9e10ea05c9d2ecabd1f29e33d462fc38019cf
Rethurd <k.grzegorek1@gmail.com>
2019-04-05 18:20:44 +0200

    Fetch hotModels

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,25 @@ import '../styles/styles.scss';
 class App extends Component {
   constructor(props){
     super(props);
-    
+    this.state={
+      carModels:[]
+    }
+  }
+
+  
+  componentDidMount(){
+    this.fetchCarModels();
+  }
+  fetchCarModels = () =>{
+    fetch("https://stock.ssangyong.pl/api/getHotModels/")
+    .then((res)=>res.json())
+    .then((data)=>{
+      const carModels=[];
+      data.forEach(modelObject => {
+        carModels.push(modelObject.model);
+      });
+      this.setState(()=>({carModels}));
+    })
   }
 
   render() {
