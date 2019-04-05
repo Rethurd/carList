e848b1bee5d5411883b5412164886b912b081500
Rethurd <k.grzegorek1@gmail.com>
2019-04-05 18:01:15 +0200

    Initial commit - modified create-react-app boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import '../styles/styles.scss';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    
+  }
+
+  render() {
+    return (
+     <div >
+      Test
+     </div>
+    );
+  }
+}
+
+export default App;
